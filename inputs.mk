9a01f63379e9a43fc3bdde7e92fe0c9df0d3cc4f
Yao Zheng <zhengyao@vt.edu>
2017-08-31 20:03:15 -1000

    Publish on Thu Aug 31 20:03:15 HST 2017

--- inputs.mk
+++ inputs.mk
@@ -9,21 +9,15 @@ S3_BUCKET                     :=
 ######       variables are expanded when use ##########
 RSYNC_DATA_EXCLUDE            =
 
-###### The parameters passed via the make command #####
-###### specifying the folders to publish ##############
-###### the webpages and materials. ####################
-PUBLISH_WEBPAGES_DIR          :=
-PUBLISH_MATERIALS_DIR         :=
-
 ###### Add the folders ready to be published to #######
 ###### the list. The default folders in the ###########
 ###### template are: "report conf jnl slides." ########
 PROJECT_DOCS_READY            :=
 
-###### Add the folders ready to be packed to ##########
-###### the list. The default folders in the ###########
-###### template are: "report conf jnl slides." ########
-PROJECT_DOCPACS_READY         :=
-
+###### The parameters passed via the make command #####
+###### specifying the folders to publish ##############
+###### the webpages and materials. ####################
+PUBLISH_WEBPAGES_DIR          :=
+PUBLISH_MATERIALS_DIR         :=
 ###### Set this flag when the webpage is ready. #######
 PROJECT_WEBPAGES_READY        :=
