2c9b282aec41c982fb34c887f0123e0c66fdc0d1
Yao Zheng <zheng.iao@icloud.com>
2017-12-02 20:49:22 -1000

    switch to hugo for website management

--- inputs.mk
+++ inputs.mk
@@ -1,5 +1,2 @@
 ###### Set the remote git repo and branch. ############
 GITHUB_REPO                   :=
-
-###### Set this flag when the webpage is ready. #######
-COURSE_WEBPAGES_READY         :=
