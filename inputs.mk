c970f51426eca79349cafb181b798050e559fc95
Yao Zheng <zheng.iao@icloud.com>
2017-12-05 16:04:56 -1000

    Update inputs.mk

--- inputs.mk
+++ inputs.mk
@@ -4,8 +4,11 @@ GITHUB_REPO                   =
 ###### Set the remote aws s3 bucket for data. #########
 S3_DATA_BUCKET                =
 
-###### Add documents to compiled and published ########
-###### Examples: report conf jnl slides        ########
+###### Add documents to compiled ######################
+###### Examples: slides conf jnl prpsl ################
 DOCS_TO_COMPILE               =
-DOCS_TO_PUB_VIA_S3            =
-DOCS_TO_PUB_VIA_DR            =
+
+###### Add extensions to publish ######################
+###### Examples: pdf ipynb md #########################
+EXTS_TO_PUB_VIA_S3            =
+EXTS_TO_PUB_VIA_GIT           =
